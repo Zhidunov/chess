5b96e30de8999263f28d2c07488193923200a309
Nikita Zhidunov <zhidunov@gmail.com>
2021-03-21 15:40:59 +0300

    Add detecting game over and showing the result

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import React, { useEffect, useState } from "react"
-import { gameSubject, initGame } from "src/Components/Game"
+import { gameSubject, initGame, resetGame } from "src/Components/Game"
 import Board from "src/Components/Board"
 import styled from "styled-components"
 
 const App = () => {
   const [board, setBoard] = useState([])
+  const [isGameOver, setIsGameOver] = useState()
+  const [result, setResult] = useState()
 
   useEffect(() => {
     initGame()
-    const subscribe = gameSubject.subscribe((game) => setBoard(game.board))
+    const subscribe = gameSubject.subscribe((game) => {
+      setBoard(game.board)
+      setIsGameOver(game.isGameOver)
+      setResult(game.result)
+    })
 
     return () => subscribe.unsubscribe()
   }, [])
 
   return (
     <Root>
+      {isGameOver && (
+        <GameOverText>
+          Game Over
+          <NewGameButton
+            onClick={() => {
+              resetGame()
+            }}
+          >
+            <VerticalText>NEW GAME</VerticalText>
+          </NewGameButton>
+        </GameOverText>
+      )}
       <BoardContainer>
         <Board board={board} />
       </BoardContainer>
+      {result && <VerticalText>{result}</VerticalText>}
     </Root>
   )
 }
@@ -34,4 +53,26 @@ const Root = styled.div`
 const BoardContainer = styled.div`
   width: 600px;
   height: 600px;
+`
+const GameOverText = styled.h2`
+  text-orientation: upright;
+  writing-mode: vertical-lr;
+  font-family: sans-serif;
+  padding: 10px;
+  color: white;
+`
+const VerticalText = styled.span`
+  text-orientation: upright;
+  writing-mode: vertical-lr;
+  font-family: sans-serif;
+  padding: 10px;
+  color: white;
+`
+const NewGameButton = styled.button`
+  margin-top: 20px;
+  cursor: pointer;
+  background-color: rgb(63, 63, 63);
+  border: 2px solid white;
+  border-radius: 10px;
+  outline: none;
 `
--- src/Components/Game.js
+++ src/Components/Game.js
@@ -2,11 +2,20 @@ import * as Chess from "chess.js"
 import { BehaviorSubject } from "rxjs"
 
 let promotion = "rnb2bnr/pppPkppp/8/4p3/7q/8/PPPP1PPP/RNBQKBNR w KQ - 1 5"
-const chess = new Chess(promotion)
+let staleMate = "4k3/4P3/4K3/8/8/8/8/8 b - - 0 78"
+let checkMate = "rnb1kbnr/pppp1ppp/8/4p3/5PPq/8/PPPPP2P/RNBQKBNR w KQkq - 1 3"
+let insificcientMaterial = "k7/8/n7/8/8/8/8/7K b - - 0 1"
+
+const chess = new Chess(staleMate)
 
 export const gameSubject = new BehaviorSubject()
 
 export const initGame = () => {
+  updateGame()
+}
+
+export const resetGame = () => {
+  chess.reset()
   updateGame()
 }
 
@@ -39,9 +48,32 @@ export const move = (from, to, promotion) => {
 }
 
 export const updateGame = (pendingPromotion) => {
+  const isGameOver = chess.game_over()
   const newGame = {
     board: chess.board(),
     pendingPromotion,
+    isGameOver,
+    result: isGameOver ? getGameResult() : null,
   }
+
   gameSubject.next(newGame)
+}
+
+const getGameResult = () => {
+  if (chess.in_checkmate()) {
+    const winner = chess.turn() === "w" ? "BLACK" : "WHITE"
+    return `CHECKMATE - WINNER - ${winner}`
+  } else if (chess.in_draw()) {
+    let reason = "50 - MOVES - RULE"
+    if (chess.in_stalemate()) {
+      reason = "STALEMATE"
+    } else if (chess.in_threefold_repetition()) {
+      reason = "REPETITION"
+    } else if (chess.insufficient_material()) {
+      reason = "INSUFFICIENT MATERIAL"
+    }
+    return `DRAW - ${reason}`
+  } else {
+    return "UNKNOWN REASON"
+  }
 }
